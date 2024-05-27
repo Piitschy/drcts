127a766a19a8da9b8b20e8c79563905b381b8a8a
Jan <jan-d.pietsch@live.de>
2024-05-27 18:31:43 +0200

    test compliete

--- internal/directus/diff_test.go
+++ internal/directus/diff_test.go
@@ -1,9 +1,7 @@
 package directus_test
 
 import (
-	"io"
-	"os"
-	"path/filepath"
+	"fmt"
 	"testing"
 
 	"github.com/Piitschy/drctsdm/internal/directus"
@@ -34,41 +32,95 @@ func TestGetDiffNoDifference(t *testing.T) {
 }
 
 func TestGetDiffWithDifference(t *testing.T) {
-	ctx, container, base := NewDirectusContainer(t, "latest")
-	defer container.Terminate(ctx)
+	baseCtx, baseContainer, baseDirectus := NewDirectusContainerWithCollection(t, "latest", LoadTestCollection(t, "article.json"), []*directus.Field{LoadTestField(t, "id_field.json")})
+	defer baseContainer.Terminate(baseCtx)
 
-	err := base.Login(adminEmail, adminPassword)
+	targetCtx, targetContainer, targetDirectus := NewDirectusContainer(t, "latest")
+	defer targetContainer.Terminate(targetCtx)
+
+	err := baseDirectus.Login(adminEmail, adminPassword)
+	err = targetDirectus.Login(adminEmail, adminPassword)
 	if err != nil {
 		t.Fatalf("Failed to login: %s", err)
 	}
 
-	f, err := os.Open(filepath.Join("..", "..", "test", "testdata", "article.json"))
-	if err != nil {
-		t.Fatalf("Failed to open file: %s", err)
-	}
-	defer f.Close()
-	bytes, err := io.ReadAll(f)
-	if err != nil {
-		t.Fatalf("Failed to read file: %s", err)
-	}
-
-	c, err := directus.UnmarshalCollection(bytes)
-	if err != nil {
-		t.Fatalf("Failed to unmarshal collection: %s", err)
-	}
-
-	err = base.CreateCollection(c)
-	if err != nil {
-		t.Fatalf("Failed to create collection: %s", err)
-	}
-
-	s, err := base.GetSnapshot()
+	s, err := baseDirectus.GetSnapshot()
 	if err != nil {
 		t.Fatalf("Failed to get snapshot: %s", err)
 	}
 
-	_, err = base.GetDiff(s, true)
+	diff, err := targetDirectus.GetDiff(s, true)
 	if err != nil {
 		t.Fatalf("Failed to get diff: %s", err)
+	}
+
+	if diff == nil {
+		t.Fatalf("Diff should not be nil")
+	}
+}
+
+func TestApplyDiff(t *testing.T) {
+	baseCtx, baseContainer, baseDirectus := NewDirectusContainerWithCollection(t, "latest", LoadTestCollection(t, "article.json"), []*directus.Field{LoadTestField(t, "id_field.json")})
+	defer baseContainer.Terminate(baseCtx)
+
+	targetCtx, targetContainer, targetDirectus := NewDirectusContainer(t, "latest")
+	defer targetContainer.Terminate(targetCtx)
+
+	err := baseDirectus.Login(adminEmail, adminPassword)
+	err = targetDirectus.Login(adminEmail, adminPassword)
+	if err != nil {
+		t.Fatalf("Failed to login: %s", err)
+	}
+
+	s, err := baseDirectus.GetSnapshot()
+	if err != nil {
+		t.Fatalf("Failed to get snapshot: %s", err)
+	}
+
+	diff, err := targetDirectus.GetDiff(s, true)
+	if err != nil {
+		t.Fatalf("Failed to get diff: %s", err)
+	}
+
+	if diff == nil {
+		t.Fatalf("Diff should not be nil")
+	}
+
+	tSchemaOriginal, err := targetDirectus.GetSnapshot()
+	if err != nil {
+		t.Fatalf("Failed to get snapshot: %s", err)
+	}
+
+	fmt.Println("len Collections:", len(tSchemaOriginal.Collections))
+
+	// Apply the diff
+	err = targetDirectus.ApplyDiff(diff)
+	if err != nil {
+		t.Fatalf("Failed to apply diff: %s", err)
+	}
+
+	tSchema, err := targetDirectus.GetSnapshot()
+	if err != nil {
+		t.Fatalf("Failed to get snapshot: %s", err)
+	}
+
+	fmt.Println("len Collections:", len(tSchema.Collections))
+
+	if len(tSchemaOriginal.Collections) == len(tSchema.Collections) {
+		t.Fatalf("Collections should not be equal")
+	}
+
+	baseArticles, err := baseDirectus.GetCollection("articles")
+	targetArticles, err := targetDirectus.GetCollection("articles")
+	if err != nil {
+		t.Fatalf("Failed to get collection: %s", err)
+	}
+
+	if baseArticles.Collection != targetArticles.Collection {
+		t.Fatalf("Collections should be equal")
+	}
+
+	if len(baseArticles.Fields) != len(targetArticles.Fields) {
+		t.Fatalf("Fields should be equal")
 	}
 }
